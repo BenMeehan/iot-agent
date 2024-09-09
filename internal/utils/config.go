35fb6b899b97215ee6964c279d1b11e3b5a1bc8e
Ben Meehan <benmeehan111@gmail.com>
2024-09-09 21:36:56 +0530

    added loading device info from file and registering device

--- internal/utils/config.go
+++ internal/utils/config.go
@@ -1,52 +1,60 @@
 package utils
 
 import (
-	"io"
 	"os"
 
 	"github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v2"
 )
 
-// Config represents the YAML configuration file structure
 type Config struct {
-	MQTT     MQTTConfig               `yaml:"mqtt"`
-	Services map[string]ServiceConfig `yaml:"services"`
+	MQTT struct {
+		Broker        string `yaml:"broker"`
+		ClientID      string `yaml:"client_id"`
+		CACertificate string `yaml:"ca_certificate"`
+	} `yaml:"mqtt"`
+
+	Identity struct {
+		DeviceFile string `yaml:"device_file"`
+	} `yaml:"identity"`
+
+	Services struct {
+		Registration struct {
+			Topic            string `yaml:"topic"`
+			Enabled          bool   `yaml:"enabled"`
+			DeviceSecretFile string `yaml:"device_secret_file"`
+			QOS              int    `yaml:"qos"`
+			DeviceIDFile     string `yaml:"deviceid_file"`
+		} `yaml:"registration"`
+
+		Heartbeat struct {
+			Topic    string `yaml:"topic"`
+			Enabled  bool   `yaml:"enabled"`
+			Interval int    `yaml:"interval"`
+			QOS      int    `yaml:"qos"`
+		} `yaml:"heartbeat"`
+	} `yaml:"services"`
 }
 
-type MQTTConfig struct {
-	Broker   string `yaml:"broker"`
-	ClientID string `yaml:"client_id"`
-	CAFile   string `yaml:"ca_certificate"`
-}
-
-type ServiceConfig struct {
-	Topic    string `yaml:"topic"`
-	Enabled  bool   `yaml:"enabled"`
-	Interval int    `yaml:"interval"`
-}
-
-// LoadConfig loads the YAML configuration file and unmarshals it into a Config struct
+// LoadConfig loads the YAML configuration file
 func LoadConfig(filename string) (*Config, error) {
+	logrus.Infof("Loading configuration from file: %s", filename)
+
 	file, err := os.Open(filename)
 	if err != nil {
-		logrus.WithError(err).Error("Unable to open config file")
+		logrus.Errorf("Error opening config file: %v", err)
 		return nil, err
 	}
 	defer file.Close()
 
-	bytes, err := io.ReadAll(file)
-	if err != nil {
-		logrus.WithError(err).Error("Error reading config file")
-		return nil, err
-	}
-
 	var config Config
-	if err := yaml.Unmarshal(bytes, &config); err != nil {
-		logrus.WithError(err).Error("Error parsing config YAML")
+	decoder := yaml.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		logrus.Errorf("Error decoding config file: %v", err)
 		return nil, err
 	}
 
-	logrus.Info("Configuration loaded successfully")
+	logrus.Infof("Configuration loaded successfully")
 	return &config, nil
 }
