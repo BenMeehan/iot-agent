535b47181bdd1762cc70fd0806465ef798683b65
Ben Meehan <benmeehan111@gmail.com>
2025-01-27 22:45:43 +0530

    fixed registration service

---
+++ internal/models/registration.go
@@ -0,0 +1,30 @@
+package models
+
+import "encoding/json"
+
+// RegistrationPayload represents the standardized structure for device registration.
+type RegistrationPayload struct {
+	// ClientID is the unique identifier for the client.
+	ClientID string `json:"client_id,omitempty"`
+
+	// DeviceID is the unique identifier for the device.
+	DeviceID string `json:"id,omitempty"`
+
+	// Name represents the name of the device.
+	Name string `json:"name,omitempty"`
+
+	// OrgID represents the organization ID to which the device belongs.
+	OrgID string `json:"org_id,omitempty"`
+
+	// Metadata contains additional information about the device, in key-value pairs.
+	Metadata json.RawMessage `json:"metadata,omitempty"`
+}
+
+// RegistrationResponse represents the response structure from the server after a successful device registration.
+type RegistrationResponse struct {
+	// DeviceID is the unique identifier assigned to the device upon successful registration.
+	DeviceID string `json:"device_id"`
+
+	// JWTToken is the JSON Web Token provided for the device to authenticate future requests.
+	JWTToken string `json:"jwt_token"`
+}
--- pkg/file/file.go
+++ pkg/file/file.go
@@ -10,7 +10,9 @@ import (
 // FileOperations defines methods for reading from and writing to files.
 type FileOperations interface {
 	ReadFile(filePath string) (string, error)
+	ReadFileRaw(filePath string) ([]byte, error)
 	WriteFile(filePath string, data string) error
+	WriteFileRaw(filePath string, data []byte) error
 	WriteJsonFile(filePath string, data any) error
 }
 
@@ -35,9 +37,23 @@ func (fs *FileService) ReadFile(filePath string) (string, error) {
 	return string(data), nil
 }
 
+// ReadFileRaw reads the contents of the file at filePath and returns it as a byte array.
+func (fs *FileService) ReadFileRaw(filePath string) ([]byte, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+}
+
 // WriteFile writes the data string to the file at filePath.
 func (fs *FileService) WriteFile(filePath string, data string) error {
 	return os.WriteFile(filePath, []byte(data), 0644)
+}
+
+// WriteFile writes the data string to the file at filePath.
+func (fs *FileService) WriteFileRaw(filePath string, data []byte) error {
+	return os.WriteFile(filePath, data, 0644)
 }
 
 // WriteJsonFile writes the json data string to the json file at filePath.
